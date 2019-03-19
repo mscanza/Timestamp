5e94af7c98c35bd0068e4eabf6b61f310f3b1349
Glitch (fcc-project1-timestamp) <none>
2019-03-19 21:00:45 +0000

    👷🌎 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,44 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/api/timestamp/:date_string?', function(req,res,next) {
+req.date = req.params.date_string ? new Date(req.params.date_string) : new Date();
+if (req.date == "Invalid Date" && new Date(Number(req.params.date_string)) == "Invalid Date") {
+res.json({"unix": null, "utc": "Invalid Date"})
+} else if (new Date(Number(req.params.date_string)) != "Invalid Date") {
+req.date = new Date(Number(req.params.date_string));
+}
+  next();
+}, function(req,res) {
+
+res.json({"unix":req.date.getTime() ,"utc": req.date.toUTCString()})
+})
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
